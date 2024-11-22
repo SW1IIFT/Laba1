dace5bcc3d5648e65669e3906417d5a75194f589
Khazanovich Lev <levkhazanovich335@gmail.com>
2024-11-22 10:42:25 +0500

    Second commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+/*vector<string> &input(vector<string> *array1){
+    string s;
+    //vector<string> array();
+    ifstream file("VanaLeva.txt");
+    while(getline(file, s)){
+        array1.push_back(s);
+
+    }
+    file.close();
+}*/
+void pupunt(){
+
+}
+
+void mumunt(){}
+void bubunt(){}
+int main()
+{
+    /*vector<string> &array  =input(&array);
+    cout << array.size();*/
+
+    pupunt();
+    mumunt();
+    bubunt();
+
+    return 0;
+}
